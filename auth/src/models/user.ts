bc008f824a751ca73e1144277cb5521882313bcd
Alexandr <a1x40v@gmail.com>
2020-12-18 20:30:29 +0300

    Fix #1 issue for TS and mongoose workflow: type checking for User creation

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
 
+/*  An interface that describes the properties
+    that are requried to create a new User */
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+/*  An inteface that describes the properties
+    that a User Model has */
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userShema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +24,10 @@ const userShema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userShema);
+userShema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userShema);
 
 export { User };
