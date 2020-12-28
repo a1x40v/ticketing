28c6a969c57dd8d71814dea9853b0f31dfcdfd30
Alexandr <a1x40v@gmail.com>
2020-12-28 07:38:53 +0300

    Add NATS support for Ticket Microservice

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats/nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -67,4 +68,17 @@ it('creates a ticket with valid inputs', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].title === title);
   expect(tickets[0].price === price);
+});
+
+it('publishes an event', async () => {
+  const title = 'title';
+  const price = 10;
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({ title, price })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
 } from '@achtickets/common';
 
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../nats/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats/nats-wrapper';
 
 const router = express.Router();
 
@@ -37,6 +39,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id!,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
